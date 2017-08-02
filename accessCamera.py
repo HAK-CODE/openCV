c0b39ad34cb37acba8849c293506b2364aeef3ed
hak <hammad4898@gmail.com>
2017-08-02 16:05:00 +0500

    camera access validation on videocapture object.

--- accessCamera.py
+++ accessCamera.py
@@ -4,11 +4,14 @@ import cv2
 #get stream from system camera
 stream = cv2.VideoCapture(0)
 
-while(True):
-	ret, frame = stream.read()				#read stream from camera
-	cv2.imshow('frame', frame)				#render stream on console
-	if cv2.waitKey(1) & 0xFF == ord('q'):	#user input to quit
-		break
+if stream.isOpened():
+	while(True):
+		ret, frame = stream.read()				#read stream from camera
+		cv2.imshow('frame', frame)				#render stream on console
+		if cv2.waitKey(1) & 0xFF == ord('q'):	#user input to quit
+			break
+else:
+	print('Stream not found.')
 
 stream.release()
 cv2.destroyAllWindows()
