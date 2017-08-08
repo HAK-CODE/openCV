d5b92b775ca46b55ade6fe4f299ea84301a51b16
hak <hammad4898@gmail.com>
2017-08-08 17:29:15 +0500

    Adding commits for showimagegrayscale

--- showgrayscaleimage.py
+++ showgrayscaleimage.py
@@ -7,6 +7,10 @@ img = cv2.imread('files/watch.jpg', cv2.IMREAD_GRAYSCALE)
 # we can also use 0 as second parameter for grayscale image
 #img = cv2.imread('files/watch.jpg', 0)
 
+# first param is used for setting the title of image show
 cv2.imshow('image', img)
+
+# wait for any key to press
 cv2.waitKey(0)
+
 cv2.destroyAllWindows()
